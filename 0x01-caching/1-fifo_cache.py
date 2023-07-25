39c7024001ce18934aa96bb0d37b51c813712ebf
Goldjay94 <genevieveezeh786@gmail.com>
2023-07-25 19:47:39 +0000

    added 1. FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Implementation of a basic caching system with FIFO
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFO caching"""
+
+    def put(self, key, value):
+        """Insert a value in the caching system"""
+        if not key or not value:
+            return
+
+        keys = self.cache_data.keys()
+        if len(keys) == BaseCaching.MAX_ITEMS:
+            temp_key = list(keys)[0]
+            del self.cache_data[temp_key]
+            print('DISCARD: {}'.format(temp_key))
+
+        self.cache_data[key] = value
+
+    def get(self, key):
+        """Retrieve a value from the caching system"""
+        if not key:
+            return None
+
