bef8bff4ccee0544ef9761aba84f88092f07d424
Goldjay94 <genevieveezeh786@gmail.com>
2023-07-26 17:21:12 +0000

    Task 4. MRU Caching fixed

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -6,39 +6,54 @@ from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """MRUCache class that inherits from BaseCaching
-    and is a caching system"""
-    def __init__(self) -> None:
-        super().__init__()
-        self.__access = {}
+    """MRU caching"""
 
-    def put(self, key, item):
-        """Add an item to the cache"""
-        if key is not None and item is not None:
-            if len(self.cache_data) == self.MAX_ITEMS\
-                    and key not in self.cache_data.keys():
-                key_to_discard = self.get_lrk()
-                del self.cache_data[key_to_discard]
-                del self.__access[key_to_discard]
-                print(f"DISCARD: {key_to_discard}")
-            if key not in self.cache_data:
-                self.__access[key] = datetime.now()
-            self.cache_data[key] = item
+    def __init__(self):
+        """MRU Constructor"""
+        super().__init__()
+        self.keys_orders = []
+
+    def _add_element(self, key, value):
+        """
+            Add an element to cache system and save the key
+            to keep the cache order level
+        """
+        if key in self.keys_orders:
+            self.keys_orders.remove(key)
+
+        self.keys_orders.append(key)
+        self.cache_data[key] = value
+
+    def put(self, key, value):
+        """add a value in the caching system"""
+        if not key or not value:
+            return
+
+        if self.cache_data.get(key):
+            self._add_element(key, value)
+            return
+
+        keys = list(self.cache_data.keys())
+        if len(keys) == BaseCaching.MAX_ITEMS:
+            remove_key = self.keys_orders[-1]
+            self.keys_orders.pop(-1)
+            print('DISCARD: {}'.format(remove_key))
+            del self.cache_data[remove_key]
+            self._add_element(key, value)
+            return
+
+        self._add_element(key, value)
 
     def get(self, key):
-        """Get an item by key"""
-        if key is not None and key in self.cache_data:
-            if key in self.__access:
-                self.__access[key] = datetime.now()
-            return self.cache_data[key]
-        return None
+        """Retrieve a value"""
+        if not key:
+            return None
 
-    def get_lrk(self):
-        """Get the least requested key"""
-        keys = list(self.__access.keys())
-        lrk = 0
-        # print(keys)
-        for i in range(1, len(keys)):
-            if self.__access[keys[lrk]] < self.__access[keys[i]]:
-                lrk = i
-        return keys[lrk]
+        el = self.cache_data.get(key)
+        if not el:
+            return None
+
+        # Reorder the keys
+        self.keys_orders.remove(key)
+        self.keys_orders.append(key)
+        return self.cache_data.get(key)
