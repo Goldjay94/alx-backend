2723fa9303d3b90fc066660c8df9605749113610
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-01 19:07:20 +0000

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,41 +1,25 @@
 #!/usr/bin/env python3
-"""4-app module"""
-
-
-from flask import Flask, render_template, request
+""" 4-app module """
+from typing import Union
+from flask import Flask, request
 from flask_babel import Babel
-
-def get_locale():
-    """ get_locale """
-    locale = request.args.get('locale')
-    if locale and locale in app.config['LANGUAGES']:
-        print(locale)
-        return locale
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-class Config():
-    """ Config class for Babel"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-    # BABEL_LOCALE_SELECTOR = get_locale
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
 babel = Babel(app)
-# Either define it this like  or with it defined in the
-# config object
-# babel.init_app(app, locale_selector=get_locale)
-babel.init_app(app, locale_selector=get_locale)
+
+app.config.from_object(Config)
+app.register_blueprint(app_routes)
 
 
-@app.route('/')
-def index():
-    """ index """
-    return render_template("4-index.html", get_locale=get_locale)
+@babel.localeselector
+def get_locale() -> Union[str, None]:
+    """ get locale """
+    locale = request.args.get('locale')
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', debug=True
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
