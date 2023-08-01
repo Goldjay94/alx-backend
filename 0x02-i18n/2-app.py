6577c85295ff459e019291f573bc1d3db1a0f857
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-01 15:34:37 +0000

    corrected task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """ 2-app module """
+
 from flask import Flask, request
 from flask_babel import Babel
 from routes.routes_2 import app_routes
 from config import Config
+
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
@@ -18,6 +28,14 @@ def get_locale() -> str:
     """ Determine best match for supported languages
     """
     return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('2-index.html')
 
 
 if __name__ == "__main__":
