15c3c3397250dfebf2d7217dd947929fd2cede28
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-01 19:04:28 +0000

    corrected task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -5,48 +5,37 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
-class Config(object):
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-# configure the flask app
-app = Flask(__name__)
-app.config.from_object(Config)
-app.url_map.strict_slashes = False
-babel = Babel(app)
-
-
-@babel.localeselector
 def get_locale():
-    """_summary_
-
-    Returns:
-            _type_: _description_
-    """
+    """ get_locale """
     locale = request.args.get('locale')
-    if locale in app.config['LANGUAGES']:
+    if locale and locale in app.config['LANGUAGES']:
         print(locale)
         return locale
-
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
+class Config():
+    """ Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    # BABEL_LOCALE_SELECTOR = get_locale
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+# Either define it this like  or with it defined in the
+# config object
 # babel.init_app(app, locale_selector=get_locale)
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route('/')
 def index():
-    """_summary_
-    """
-    return render_template('4-index.html')
+    """ index """
+    return render_template("4-index.html", get_locale=get_locale)
 
 
 if __name__ == '__main__':
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run(host='0.0.0.0', port='5000', debug=True
