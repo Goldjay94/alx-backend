4bacb8643c6189d1d7b805c8147032fd204e425b
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-01 19:12:41 +0000

    fixed error task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python3
 """ 4-app module """
-from typing import Union
-from flask import Flask, request
-from flask_babel import Babel
+from flask_babel import Babel,
+from flask import Flask, render_template, request, flash
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
+
+class Config(object):
+    """ Configuration Class for Babel """
+
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
-app.register_blueprint(app_routes)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world() -> str:
+    """Renders a Basic Template for Babel Implementation"""
+    return render_template("4-index.html")
 
 
 @babel.localeselector
-def get_locale() -> Union[str, None]:
-    """ get locale """
-    locale = request.args.get('locale')
-    if locale and locale in Config.LANGUAGES:
+def get_locale() -> str:
+    """Select a language translation to use for that request"""
+    locale = request.args.get("locale")
+    if locale and locale in app.config['LANGUAGES']:
         return locale
-    return request.accept_languages.best_match(Config.LANGUAGES)
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run()
